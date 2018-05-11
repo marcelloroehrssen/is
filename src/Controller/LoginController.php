0ac0855cbc183d02d967b9dba8a8c746aac27e20
marcelloroehrssen <marcello.roehrssen@gmail.com>
2018-05-11 22:05:03 +0200

    Sistemati assets

---
+++ src/Controller/LoginController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+
+class LoginController extends Controller
+{
+    /**
+     * @Route("/login", name="login")
+     */
+    public function index()
+    {
+        return $this->render('login/index.html.twig', [
+            'controller_name' => 'LoginController',
+        ]);
+    }
+}
